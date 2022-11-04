65dd3f76c6be7fc51af0ebe9ac300b65f1ecc1a1
omikeflies <omikeflies@gmail.com>
2022-11-04 20:50:43 +0100

    Setting Social Media links

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,13 +1,11 @@
 import { BsTwitter} from "react-icons/bs";
+import {SlSocialSoundcloud} from "react-icons/sl"
 
 export function SocialMedia() {
   return (
     <ul>
-      <li>
-        <a href='https://twitter.com/MiloBembe'>
-          <BsTwitter />
-        </a>
-      </li>
+      <li className="SocialMedia">  <a href='https://twitter.com/MiloBembe'>  <BsTwitter />  </a> </li>
+        <li className="SocialMedia"><a href="https://soundcloud.com/milobembe"><SlSocialSoundcloud/></a>  </li>
     </ul>
   );
 }
